31b51fa7844d67cbccc48d4a6dd155d06aa17c52
zjaaos0509 <zjaaos0509@gmail.com>
2018-10-21 17:37:37 +0900

    10월 21일 PP08.SingletonTM

---
+++ PP08.SingletonTM/Game.h
@@ -0,0 +1,23 @@
+﻿#include "TextureManager.h"
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+	int m_currentFrame;
+	TextureManager m_textureManager;
+	//SDL_Texture* m_pTexture; // the new SDL_Texture variable
+	//SDL_Rect m_sourceRectangle; // ¿øº» »ç°¢Çü 
+	//SDL_Rect m_destinationRectangle; // ´ë»ó »ç°¢Çü 
+};
