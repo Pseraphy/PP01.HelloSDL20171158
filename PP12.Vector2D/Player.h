2922c44a2a8981b2ff0452e03da4b47259997ac7
zjaaos0509 <zjaaos0509@gmail.com>
2018-11-01 18:38:18 +0900

    11월 1일 PP12.Vector2D

---
+++ PP12.Vector2D/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "SDLGameObject.h"
+
+
+
+class Player : public SDLGameObject
+{
+public:
+	Player(const LoaderParams* pParams);
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+	/*void load(int x, int y, int width, int height,
+	std::string textureID);
+	void draw(SDL_Renderer* pRenderer);
+	void update();
+	void clean();*/
+};
